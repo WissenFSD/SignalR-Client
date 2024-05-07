f52db2f82381ec3a68c40b9a91c8237c98a949dc
BRIGHT <BRIGHT@DESKTOP-5789TGK>
2024-05-07 21:20:07 +0300

    Add project files.

---
+++ Signal-R Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Signal_R_Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
